d4710279c58ec8382e76722c78e26d64146b3acf
BLoushi52 <k.albloushii@hotmail.com>
2022-10-02 20:37:27 +0300

    solved questions

--- hr_pro.py
+++ hr_pro.py
@@ -12,8 +12,10 @@ class Employee:
          return f"Name: {self.name}, Age: {self.age}, Salary: {self.salary}, Working Years: {self.employment_years}"
 
 employee1 = Employee("Khaled", 26, 100, 4)
+employee2 = Employee("Moh", 5, 300, 7)
 
-print(employee1)      #Testing
+
+# print(employee1)      #Testing
 
 
 
@@ -30,7 +32,30 @@ class Manager(Employee):
          return f"Name: {self.name}, Age: {self.age}, Salary: {self.salary}, Working Years: {self.employment_years}, Bonus: {int(self.get_bonus())}"
 
 manager1 = Manager("Khaledd", 26, 200, 4, 0.1)
-print(manager1)     #testing
+# print(manager1)     #Testing
+
+
+
+employees=[employee1, employee2]
+managers=[manager1]
+
+
+
+print(f"\nWelcome to HR Pro\nOptions:\n")
+print(f"1. Show Employees \n2. Show Managers \n3. Add An Employee \n4. Add A Manager \n5. Exit\n")
+x = 4
+while x >= 4:
+
+    if int(input("What would you like to do? ")) == 1:
+        print(employees)
+    if x == 2:
+        print(managers)
+
+
+
+
+
+
 
 
 '''
